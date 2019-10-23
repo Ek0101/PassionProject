c5e1a346bef72c936d689f2cb9eab954100a2858
Ek0101 <evanzaman.khan@gmail.com>
2019-10-23 17:42:43 -0400

    Spring Boot Project created

---
+++ src/main/java/com/blooddrivebd/blooddrivebd/BloodDriveBdApplication.java
@@ -0,0 +1,13 @@
+package com.blooddrivebd.blooddrivebd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDriveBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDriveBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blooddrivebd/blooddrivebd/BloodDriveBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blooddrivebd.blooddrivebd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDriveBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
